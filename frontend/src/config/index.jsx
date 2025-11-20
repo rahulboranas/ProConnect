a3084484f056992b3c660939c5f32e7a77671c7f
rahulboranas <rahulmhtcet@gmail.com>
2025-11-20 20:34:30 +0530

    commitfive

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const {default:axios}= require("axios");
-// export const BASE_URL ="http://localhost:9090"
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const BASE_URL ="https://proconnect-1-q9s5.onrender.com"
+
 
 export const clientServer = axios.create({
     baseURL : BASE_URL,
